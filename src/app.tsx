5a7f0db471593ffdbf8981dea7fccf795dfade09
v_wlgowang <v_wlgowang@tencent.com>
2020-09-04 19:29:53 +0800

    updata

--- src/app.tsx
+++ src/app.tsx
@@ -7,7 +7,7 @@ import '@src/global.scss'
 import {globalVar} from '@utils/publick'
 import {c} from '@src/utils'
 // import _ from 'lodash'
-
+const a = 123
 // 注册全局变量，解决按需引入polyfill的缺陷
 globalVar()
 
