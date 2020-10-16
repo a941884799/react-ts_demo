33daffe2cc3cf4d673d3fc83fe52ee0040496d9e
v_wlgowang <v_wlgowang@tencent.com>
2020-10-16 17:38:14 +0800

    更新

--- src/pages/Wlg/ReduxDemo/index.tsx
+++ src/pages/Wlg/ReduxDemo/index.tsx
@@ -3,19 +3,27 @@
  * WangLonggang 的 ReduxDemo
  * */
 import React from 'react';
-// import {useDispatch, useSelector} from 'react-redux'
-// import {Types, add} from '@store/actions/common'
-import { PageHeader, Tag } from 'antd';
-import './index.scss';
+import { useDispatch, useSelector } from 'react-redux';
+import { Types, add } from '@store/actions/common';
+import { Space, Button, DatePicker } from 'antd';
 
-/**
- * 页面主组件
- */
 const ReduxDemo = () => {
+	const dispatch = useDispatch();
+	const todos: [] = useSelector((store: any) => store.todos);
+	const counter: number = useSelector((store: any) => store.counter);
+	console.log('Home 重新渲染');
 	return (
-		<div className="page-WlgReduxDemo">
-			<PageHeader title="ReduxDemo" subTitle="This is redux demo" tags={<Tag color="red">demo</Tag>} />
-		</div>
+		<Space direction="vertical">
+			<Button type="primary" onClick={() => dispatch({ type: Types.ADD_TODO, text: todos.length })}>
+				ADD_TODO
+			</Button>
+			<Button type="primary" onClick={() => dispatch(add)}>
+				add counter
+			</Button>
+			<DatePicker showTime />
+			<h4>todos:{todos.map(i => i)}</h4>
+			<h4>counter: {counter} </h4>
+		</Space>
 	);
 };
 
