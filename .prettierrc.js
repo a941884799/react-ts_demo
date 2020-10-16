1c7e6e157f1ff9c0c9072e4c7839769361681aff
v_wlgowang <v_wlgowang@tencent.com>
2020-10-16 14:46:24 +0800

    update

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,11 @@
 module.exports =  {
-  "printWidth": 120,
-  "semi": false,
-  "singleQuote": true,
-  "trailingComma": "all",
-  "bracketSpacing": false,
-  "jsxBracketSameLine": true,
-  "arrowParens": "avoid",
-  "insertPragma": true,
-  "tabWidth": 2,
-  "useTabs": false  
+  "printWidth": 120, // 每行宽度
+  "singleQuote": true, // 使用单引号而不是双引号
+  "trailingComma": "all", // 尽可能在结尾加上逗号
+  "arrowParens": "avoid", // 取消单独的箭头函数参数周围的括号。
+  "useTabs": true, // 使用Tab而不是空格缩进行
+  // "bracketSpacing": false, // 不在对象文字中的括号之间打印空格。
+  // "jsxBracketSameLine": true, // 将多行JSX元素的>放在最后一行的末尾，而不是单独放在下一行（不适用于自闭元素）。
+  // "insertPragma": true, // 在文件顶部插入一个特殊的@format标记
+  // "tabWidth": 2, // 指定每个缩进级别的空格数(默认为2)
 };
