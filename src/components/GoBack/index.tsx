1a76323ca4734808642f26ae7abea0d3f52c3bdb
v_wlgowang <v_wlgowang@tencent.com>
2021-01-19 10:53:27 +0800

    update

--- src/components/GoBack/index.tsx
+++ src/components/GoBack/index.tsx
@@ -1,10 +1,11 @@
 import React, { ReactNode } from 'react';
 import { ArrowLeftOutlined } from '@ant-design/icons';
-const GoBack = (): ReactNode => (
+const GoBack = (props = {}): ReactNode => (
   <ArrowLeftOutlined
     className="GoBack primary-color"
-    style={{ fontSize: 16, fontWeight: 700, marginRight: 30 }}
+    style={{ fontSize: 16, fontWeight: 700, marginRight: 20 }}
     onClick={() => window.history.back()}
+    {...props}
   />
 );
 
